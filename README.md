fa27dbfab60b66ae42296e22219bdb0164fac3e7
wangsiyuan <530091512@qq.com>
2026-08-01 15:24:24 +0800

    增加第三方网络通信框架workflow 增加OpenSSL1.1.1版本的windows安装包

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # NewWorldServer
-The server of "New World"
+(最后一次更新时间2026.8.1 15:14)
+
+## 1 构建及编译
+
+本服务器目前仅支持Windows操作系统，通过cmake进行构建和编译
+
+本系统的通信框架使用sougou的workflow框架（通过源码构建，可以在github拉取仓库后，切换到windows分支，workflow对windows的支持没有linux那么好）
+
+workflow依赖OpenSSL
+
+在windows上编译workflow源码时，必须使用OpenSSL1.1.1版本，如果使用其他版本，在使用cmake构建workflow时，无法找到OpenSSL的关键库文件
+
+OpenSSL1.1.1的安装程序在env目录下可见"Win64OpenSSL-1_1_1i.msi"
+
+## 使用的第三方库
+
+spdlog
+
+workflow
+
+
+
---
+++ communicate/workflow/tutorial/tutorial-08-matrix_multiply.cc
@@ -0,0 +1,150 @@
+#include <assert.h>
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+#include "workflow/WFTaskFactory.h"
+
+namespace algorithm
+{
+
+typedef std::vector<std::vector<double>> Matrix;
+
+struct MMInput
+{
+	Matrix a;
+	Matrix b;
+};
+
+struct MMOutput
+{
+	int error;
+	size_t m, n, k;
+	Matrix c;
+};
+
+bool is_valid_matrix(const Matrix& matrix, size_t& m, size_t& n)
+{
+	m = n = 0;
+	if (matrix.size() == 0)
+		return true;
+
+	m = matrix.size();
+	n = matrix[0].size();
+	if (n == 0)
+		return false;
+
+	for (const auto& row : matrix)
+		if (row.size() != n)
+			return false;
+
+	return true;
+}
+
+void matrix_multiply(const MMInput *in, MMOutput *out)
+{
+	size_t m1, n1;
+	size_t m2, n2;
+
+	if (!is_valid_matrix(in->a, m1, n1) || !is_valid_matrix(in->b, m2, n2))
+	{
+		out->error = EINVAL;
+		return;
+	}
+
+	if (n1 != m2)
+	{
+		out->error = EINVAL;
+		return;
+	}
+
+	out->error = 0;
+	out->m = m1;
+	out->n = n2;
+	out->k = n1;
+
+	out->c.resize(m1);
+	for (size_t i = 0; i < out->m; i++)
+	{
+		out->c[i].resize(n2);
+		for (size_t j = 0; j < out->n; j++)
+		{
+			out->c[i][j] = 0;
+			for (size_t k = 0; k < out->k; k++)
+				out->c[i][j] += in->a[i][k] * in->b[k][j];
+		}
+	}
+}
+
+}
+
+using MMTask = WFThreadTask<algorithm::MMInput,
+							algorithm::MMOutput>;
+
+using namespace algorithm;
+
+void print_matrix(const Matrix& matrix, size_t m, size_t n)
+{
+	for (size_t i = 0; i < m; i++)
+	{
+		for (size_t j = 0; j < n; j++)
+			printf("\t%8.2lf", matrix[i][j]);
+
+		printf("\n");
+	}
+}
+
+void callback(MMTask *task)
+{
+	auto *input = task->get_input();
+	auto *output = task->get_output();
+
+	assert(task->get_state() == WFT_STATE_SUCCESS);
+
+	if (output->error)
+		printf("Error: %d %s\n", output->error, strerror(output->error));
+	else
+	{
+		printf("Matrix A\n");
+		print_matrix(input->a, output->m, output->k);
+		printf("Matrix B\n");
+		print_matrix(input->b, output->k, output->n);
+		printf("Matrix A * Matrix B =>\n");
+		print_matrix(output->c, output->m, output->n);
+	}
+}
+
+int main()
+{
+	using MMFactory = WFThreadTaskFactory<MMInput,
+										  MMOutput>;
+	MMTask *task = MMFactory::create_thread_task("matrix_multiply_task",
+												matrix_multiply,
+												callback);
+	auto *input = task->get_input();
+
+	input->a = {{1, 2, 3}, {4, 5, 6}};
+	input->b = {{7, 8}, {9, 10}, {11, 12}};
+
+	std::mutex mutex;
+	std::condition_variable cond;
+	bool finished = false;
+
+	Workflow::start_series_work(task,
+		[&mutex, &cond, &finished](const SeriesWork *)
+	{
+		mutex.lock();
+		finished = true;
+		cond.notify_one();
+		mutex.unlock();
+	});
+
+	std::unique_lock<std::mutex> lock(mutex);
+	while (!finished)
+		cond.wait(lock);
+	lock.unlock();
+	return 0;
+}
+
